73df7f3fc30391efaba782d2daf04c559c6080a8
higuita1498 <higuitajokers@gmail.com>
2024-08-01 10:45:24 -0500

    feat: actualizaciones de electronica pdf 7.0

--- resources/views/pdf/factura.blade.php
+++ resources/views/pdf/factura.blade.php
@@ -156,7 +156,7 @@
             @endif
             <tr>
                 <th width="10%" class="right smalltd">SEÑOR(ES)</th>
-                <td colspan="3" style="border-top: 2px solid #ccc;">{{$factura->cliente()->nombre}}</td>
+                <td colspan="3" style="border-top: 2px solid #ccc;">{{$factura->cliente()->nombre}} {{ $factura->cliente()->apellido1 }}</td>
                 <th width="22%" class="center" style="font-size: 8px"><b>FECHA DE GENERACION (DD/MM/AA)</b></th>
             </tr>
             <tr>
@@ -280,7 +280,7 @@
                     @endif
 
                     <th class="right padding-right">Total</th>
-                    <th class="center padding-right">{{Auth::user()->empresa()->moneda}}{{App\Funcion::Parsear($factura->total()->total)}} </th>
+                    <th class="center padding-right">{{Auth::user()->empresa()->moneda}}{{App\Funcion::Parsear($factura->total()->total + $factura->estadoCuenta()->saldoMesAnterior)}} </th>
                 </tr>
             </tfoot>
         </table>
