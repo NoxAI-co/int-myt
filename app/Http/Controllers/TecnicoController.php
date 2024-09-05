804df20844847a72fedb1819b66ef26f6b567e8f
Johan Carrillo <johan.carrillo@softnew.me>
2024-09-05 12:42:12 -0500

    feat: Se crea el módulo técnico y la vista para mostrar la ubicación.

--- app/Http/Controllers/TecnicoController.php
+++ app/Http/Controllers/TecnicoController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class TecnicoController extends Controller
 {
     public function __construct(){
