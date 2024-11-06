1b13e9b6dbb0600302d114a0f0a362f914b62fdf
higuita1498 <higuitajokers@gmail.com>
2024-11-06 08:28:57 -0500

    fix: no dejaba borrar vacaciones por error en la url

--- resources/views/nomina/modals/vacaciones-incap.blade.php
+++ resources/views/nomina/modals/vacaciones-incap.blade.php
@@ -455,7 +455,12 @@
 
     function destroyVacaciones(id) {
         cargando(true);
-        var url = '/empresa/nomina/liquidar-nomina/' + id + '/destroy_vacaciones';
+        if (window.location.pathname.split("/")[1] === "software") {
+            var url = '/software/empresa/nomina/liquidar-nomina/' + id + '/destroy_vacaciones';
+        }else{
+            var url = '/empresa/nomina/liquidar-nomina/' + id + '/destroy_vacaciones';
+        }
+        
         var _token = $('meta[name="csrf-token"]').attr('content');
         var i = id;
         $.post(url, {
