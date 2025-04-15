4a2f3d2e16dd3a117358082cd595fe50736e6b74
higuita1498 <higuitajokers@gmail.com>
2025-04-15 09:20:15 -0500

    fix:agregando mas minutos a que no pueda crear pagos con el mismo numero

--- app/Http/Controllers/IngresosController.php
+++ app/Http/Controllers/IngresosController.php
@@ -298,7 +298,7 @@ class IngresosController extends Controller
                 $pagoRepetido = IngresosFactura::where('factura', $factura_id)
                     ->where('pagado', $montoPago)
                     ->whereHas('ingresoRelation', function ($query) {
-                        $query->whereBetween('created_at', [now()->subSeconds(120), now()]);
+                        $query->whereBetween('created_at', [now()->subSeconds(600), now()]);
                     })
                     ->exists();
 
